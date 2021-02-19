014e872b5e66c12b422ec27ad6565b90ba693907
JeevaRamanathan <jeevaram2000@gmail.com>
2021-02-19 19:39:48 +0530

    Bus Stage Update

--- screens/BusStageRouteList.js
+++ screens/BusStageRouteList.js
@@ -7,25 +7,24 @@ export default class BusNumberRouteList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        route_id: this.props.navigation.state.params.data.value,
-        route_details: [],
-       
+      route_id: this.props.navigation.state.params.data.value,
+      route_details: [],
     };
   }
   componentDidMount() {
-      //won't work until you pass the data in previous page
+    //won't work until you pass the data in previous page
     for (var i = 0; i < this.state.route_id.length; i++) {
-        await database()
-          .ref(`/Routes/r${this.state.route_id[i]}`)
-          .on('value', (snapshot) => {
-            let arr = this.state.route_details;
-            arr.push(snapshot.val());
-            this.setState({route_details: arr});
-          });
-      }
+      database()
+        .ref(`/Routes/r${this.state.route_id[i]}`)
+        .on('value', (snapshot) => {
+          let arr = this.state.route_details;
+          arr.push(snapshot.val());
+          this.setState({route_details: arr});
+        });
+    }
   }
   render() {
-     //follow the design and flow as in BusNumberRouteList.js
+    //follow the design and flow as in BusNumberRouteList.js
     return <></>;
   }
 }
